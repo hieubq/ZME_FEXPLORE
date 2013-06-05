f1bbce7f25efb9d52a146f810dae8f0b5ab89aa1
hieubq <hieubq.vng.com.vn>
2013-06-05 16:37:33 +0700

    first commit

---
+++ ZME_FileExplore/src/com/vng/zme_file_explore/base/BasePagerActivity.java
@@ -0,0 +1,11 @@
+package com.vng.zme_file_explore.base;
+
+import com.viewpagerindicator.PageIndicator;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+public class BasePagerActivity extends FragmentActivity {
+	ViewPager mPager;
+    PageIndicator mIndicator;
+}
